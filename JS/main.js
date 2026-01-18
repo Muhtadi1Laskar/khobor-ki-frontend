3b8e09222f90e876bacb569876a7111bfa9336d2
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2026-01-19 03:17:11 +0600

    Added new font

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,5 @@
 const API_BASE_URL = 'https://khobor-ki-backend.onrender.com/api/feed';
+// const API_BASE_URL = 'http://localhost:8080/api/feed';
 const ITEMS_PER_PAGE = 10;
 
 let currentCategory = 'all';
