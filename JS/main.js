fc4d8c0381708b6cf6119846664d5961157922fe
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2026-01-30 04:36:10 +0600

    Changed the limit on each page

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 // Configuration
 const API_BASE_URL = 'https://khobor-ki-backend.onrender.com/api/feed';
 // const API_BASE_URL = 'http://localhost:8080/api/feed';
-const ITEMS_PER_PAGE = 10;
+const ITEMS_PER_PAGE = 15;
 
 // Language translations
 const translations = {
