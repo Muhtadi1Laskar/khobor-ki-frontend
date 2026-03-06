341e433704f792918bec07b36072ac9cbd3a6414
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2026-03-07 03:09:08 +0600

    Changed button text

--- JS/Utils/options.js
+++ JS/Utils/options.js
@@ -49,7 +49,7 @@ const translations = {
         articles: 'articles',
         sources: 'sources',
         topCoverage: 'Top Coverage',
-        viewAllArticles: 'View all headlines',
+        viewAllArticles: 'View all Headlines',
 
         coverageAnalysis: 'Coverage Analysis',
         learnMore: 'Learn more',
